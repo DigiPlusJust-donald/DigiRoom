26df8d3662725b9d35f996f919a221e0db0a69f1
DigiPlus_just-donald <donald.tsemo@digiplusconsulting.com>
2023-05-22 18:42:36 +0100

    french language architecture configuration

---
+++ js/script.js
@@ -0,0 +1,80 @@
+// architecture of the french language text
+const data = {
+	french: {
+		Accueil: {
+			title1: ' à développer votre entreprise',
+			boldPart1: 'Nous vous aidons',
+			content1: {
+				bannerText1:
+					'Enfin une solution taillée à vos besoins spécifiques et accessible selon votre bourse et a la demande.',
+				subBannerText1: {
+					p: 'Activités concernées:',
+					ul: {
+						li1: 'Le regroupement virtuel des personnes pour une formation a distance avec des facilitateurs connectes de par le monde.',
+						li2: 'Les réunions ou séminaires en ligne pour les personnes individuelles ou morales recherchant un cadre propice.',
+						li3: "Les certifications en ligne dans les normes prescrites par l'agence de certification.",
+						li4: 'Les rencontres privées et intimes en ligne.',
+						li5: "Les entretiens d'embauche",
+					},
+					buttonText1: 'En Savoir Plus',
+				},
+			},
+			title2: 'Il Comprend ',
+			boldPart2: 'Egalement...',
+			content21: {
+				bannerText21: 'Configuration minimale de la salle:',
+				ul: {
+					li1: 'Un cadre acoustique et climatise',
+					li2: "Un accès internet permanent et de bonne capacité avec un système de backup en cas d'interruption du principal",
+					li3: 'Une fourniture électrique permanente avec des équipements de protection et un générateur électrique pour reprise en cas de coupure de courant.',
+					li4: 'Un écran Smart TV géant pour la présentation et la vidéo conférence.',
+					li5: 'Un dispositif sophistique et intégré de Microphone and Camera contenant toute la salle.',
+					li6: "Une console centrale d'administration des sessions.",
+					li7: 'Les outils collaboratifs tels que Microsoft Teams, Zoom, Google Meet, etc.',
+					li8: 'Des interfaces de consultation de disponibilité et de gestion de réservations de la salle.',
+				},
+			},
+			content22: {
+				bannerText22: 'Promotion:',
+				ul: {
+					li: "Bénéficier de nos services gratuitement pour toutes les premières heures de vos réservations jusqu'à la fin du mois de Juin. Il suffit de nous contacter ou effectuer vous-même la réservation en ligne à travers les adresses qui vous sont fournies sur cette page.",
+				},
+			},
+		},
+		Services: {
+			buttonText: 'Effectuer Une Resèrvation',
+			title3: 'Ce que ',
+			boldPart3: 'nous vous offrons',
+			content3: {
+				bannerText:
+					"DigiPlus met à votre disposition un ensemble de salles connectés et équipées d'un dispositif de vidéoconférence et des outils de collaborations.",
+				card1: {
+					cardTitle: 'Un espace idéal pour vos réunions',
+					cardSubtitle:
+						'Vous voulez effectuer un rencontre sérieuse avec un partenaire a distance ou surplace. Vous voulez assister a une réunion ou séminaire sérieux dans un cadre propice.',
+					ul: {
+						li: "DigiRoom vous offre une salle d'une capacité de 8 places extensible a 10 pour regrouper vos apprenants dans la ville couverte.",
+					},
+				},
+				card2: {
+					cardTitle: 'Un espace calme et privé',
+					cardSubtitle:
+						"Vous voulez effectuer des rencontres ponctuelles ou récurrentes avec vos employés ou partenaires se trouvant dans la ville couverte par DigiRoom. C'est l'exemple des regroupements des commerciaux pour le suivi des activités et la revue des performances.",
+					ul: {
+						li1: 'Les rencontres privées et intimes en ligne.',
+						li2: "Les entretiens d'embauche ",
+					},
+				},
+				card3: {
+					cardTitle: 'Équipements de haute technologie disponibles',
+					cardSubtitle:
+						"Vous voulez faire bénéficier un ou plusieurs employés d’une formation ou séminaires en distance dans un cadre propice. C'est l'exemple d'un séminaire organise a l'international auquel des employés doivent participer a distance.",
+					ul: {
+						li: 'Les certifications en ligne dans les normes prescrites par l’agence de certification',
+					},
+				},
+			},
+		},
+		'A Propos': {},
+	},
+};
